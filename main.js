870c6d725e181c431c48a040d554ef305d9a125c
Dani2294 <daniel.agbato@yahoo.fr>
2021-12-29 16:24:21 +0100

    v2

--- main.js
+++ main.js
@@ -5,4 +5,6 @@ document.querySelector('#app').innerHTML = `
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
  */
+const test = document.querySelector('.mb-8');
 console.log('Hello the console !');
+console.log(test);
